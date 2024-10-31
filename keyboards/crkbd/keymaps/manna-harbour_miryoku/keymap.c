9bb1722f9c0a4a5f1ed45c5ba96b90bc9cb587c1
James Tan <james_tan97@outlook.com>
2024-10-31 01:58:09 -0400

    add input for e with french accents

--- keyboards/crkbd/keymaps/manna-harbour_miryoku/keymap.c
+++ keyboards/crkbd/keymaps/manna-harbour_miryoku/keymap.c
@@ -3,94 +3,113 @@
 
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+#include "sendstring_us_international.h"
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 
+enum custom_keycodes {
+    E_GRV = SAFE_RANGE,
+    E_ACU,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case E_GRV:
+            if (record->event.pressed) {
+                SEND_STRING(SS_ALGR(SS_TAP(X_GRV)) SS_TAP(X_E));
+            }
+            return false;
+        case E_ACU:
+            if (record->event.pressed) {
+                SEND_STRING(SS_ALGR(SS_TAP(X_E)) SS_TAP(X_E));
+            }
+            return false;
+    }
+
+    return true;
+}
+
 #ifdef OLED_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-  if (!is_keyboard_master()) {
-    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
-  }
-  return rotation;
+    if (!is_keyboard_master()) {
+        return OLED_ROTATION_180; // flips the display 180 degrees if offhand
+    }
+    return rotation;
 }
 
 enum layers { BASE, EXTRA, TAP, BUTTON, NAV, MOUSE, MEDIA, NUM, SYM, FUN };
 
 void oled_render_layer_state(void) {
-  oled_write("Layer: ", false);
+    oled_write("Layer: ", false);
 
-  switch (get_highest_layer(layer_state)) {
-  case BASE:
-    oled_write("Base", false);
-    break;
-  case EXTRA:
-    oled_write("Extra", false);
-    break;
-  case TAP:
-    oled_write("Tap", false);
-    break;
-  case BUTTON:
-    oled_write("Button", false);
-    break;
-  case NAV:
-    oled_write("Navigation", false);
-    break;
-  case MOUSE:
-    oled_write("Mouse", false);
-    break;
-  case MEDIA:
-    oled_write("Media", false);
-    break;
-  case NUM:
-    oled_write("Number", false);
-    break;
-  case SYM:
-    oled_write("Symbol", false);
-    break;
-  case FUN:
-    oled_write("Function", false);
-    break;
-  }
+    switch (get_highest_layer(layer_state)) {
+        case BASE:
+            oled_write("Base", false);
+            break;
+        case EXTRA:
+            oled_write("Extra", false);
+            break;
+        case TAP:
+            oled_write("Tap", false);
+            break;
+        case BUTTON:
+            oled_write("Button", false);
+            break;
+        case NAV:
+            oled_write("Navigation", false);
+            break;
+        case MOUSE:
+            oled_write("Mouse", false);
+            break;
+        case MEDIA:
+            oled_write("Media", false);
+            break;
+        case NUM:
+            oled_write("Number", false);
+            break;
+        case SYM:
+            oled_write("Symbol", false);
+            break;
+        case FUN:
+            oled_write("Function", false);
+            break;
+    }
 
-  oled_write("\n", false);
+    oled_write("\n", false);
 }
 
 void oled_render_led_state(void) {
-  led_t led_state = host_keyboard_led_state();
+    led_t led_state = host_keyboard_led_state();
 
-  oled_write(led_state.caps_lock ? "CAPS " : "      ", false);
-  oled_write(led_state.scroll_lock ? "SCROLL " : "       ", false);
-  oled_write("\n", false);
+    oled_write(led_state.caps_lock ? "CAPS " : "      ", false);
+    oled_write(led_state.scroll_lock ? "SCROLL " : "       ", false);
+    oled_write("\n", false);
 }
 
 void oled_render_mod_state(void) {
-  uint8_t mod_state = get_mods();
+    uint8_t mod_state = get_mods();
 
-  oled_write((mod_state & MOD_MASK_SHIFT) ? "SHIFT " : "      ", false);
-  oled_write((mod_state & MOD_MASK_CTRL) ? "CTRL " : "     ", false);
-  oled_write((mod_state & MOD_MASK_ALT) ? "ALT " : "    ", false);
-  oled_write((mod_state & MOD_MASK_GUI) ? "GUI" : "   ", false);
-  oled_write("\n", false);
+    oled_write((mod_state & MOD_MASK_SHIFT) ? "SHIFT " : "      ", false);
+    oled_write((mod_state & MOD_MASK_CTRL) ? "CTRL " : "     ", false);
+    oled_write((mod_state & MOD_MASK_ALT) ? "ALT " : "    ", false);
+    oled_write((mod_state & MOD_MASK_GUI) ? "GUI" : "   ", false);
+    oled_write("\n", false);
 }
 
 void oled_render_logo(void) {
-  static const char PROGMEM crkbd_logo[] = {
-    0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94,
-    0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4,
-    0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4,
-    0};
-  oled_write_P(crkbd_logo, false);
+    static const char PROGMEM crkbd_logo[] = {0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94, 0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4, 0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4, 0};
+    oled_write_P(crkbd_logo, false);
 }
 
 bool oled_task_user(void) {
-  if (is_keyboard_master()) {
-    oled_render_layer_state();
-    oled_render_mod_state();
-    oled_render_led_state();
-  } else {
-    oled_render_logo();
-  }
-  return false;
+    if (is_keyboard_master()) {
+        oled_render_layer_state();
+        oled_render_mod_state();
+        oled_render_led_state();
+    } else {
+        oled_render_logo();
+    }
+    return false;
 }
 
 #endif // OLED_ENABLE
--- users/manna-harbour_miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ users/manna-harbour_miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -269,7 +269,7 @@ U_NA,              KC_ALGR,           TD(U_TD_U_NUM),    TD(U_TD_U_NAV),    U_NA
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              KC_ENT,            KC_BSPC,           KC_DEL,            U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_NAV_VI \
-TD(U_TD_BOOT),     TD(U_TD_U_TAP),    TD(U_TD_U_EXTRA),  TD(U_TD_U_BASE),   U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+TD(U_TD_BOOT),     TD(U_TD_U_TAP),    E_ACU,             TD(U_TD_U_BASE),   U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              KC_LEFT,           KC_DOWN,           KC_UP,             KC_RGHT,           CW_TOGG,           \
 U_NA,              KC_ALGR,           TD(U_TD_U_NUM),    TD(U_TD_U_NAV),    U_NA,              KC_HOME,           KC_PGDN,           KC_PGUP,           KC_END,            KC_INS,            \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              KC_ENT,            KC_BSPC,           KC_DEL,            U_NP,              U_NP
@@ -300,7 +300,7 @@ U_NA,              KC_ALGR,           TD(U_TD_U_SYM),    TD(U_TD_U_MOUSE),  U_NA
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              KC_BTN2,           KC_BTN1,           KC_BTN3,           U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_MOUSE_VI \
-TD(U_TD_BOOT),     TD(U_TD_U_TAP),    TD(U_TD_U_EXTRA),  TD(U_TD_U_BASE),   U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+TD(U_TD_BOOT),     TD(U_TD_U_TAP),    E_GRV,             TD(U_TD_U_BASE),   U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              KC_MS_L,           KC_MS_D,           KC_MS_U,           KC_MS_R,           U_NU,              \
 U_NA,              KC_ALGR,           TD(U_TD_U_SYM),    TD(U_TD_U_MOUSE),  U_NA,              KC_WH_L,           KC_WH_D,           KC_WH_U,           KC_WH_R,           U_NU,              \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              KC_BTN2,           KC_BTN1,           KC_BTN3,           U_NP,              U_NP
